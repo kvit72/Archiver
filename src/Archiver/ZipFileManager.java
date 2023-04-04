8c48c9ca305d360b2f45b220c10ee26ed75aa900
kvit7 <kvit72v@yandex.ru>
2023-04-04 16:27:16 +0300

    Реализован метод createZip(Path source),  котором будет происходить архивация файла, заданного переменной source.

--- src/Archiver/ZipFileManager.java
+++ src/Archiver/ZipFileManager.java
@@ -1,6 +1,10 @@
 package Archiver;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -10,5 +14,17 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
+            ZipEntry entry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(entry);
+            try (InputStream fileInputStream = Files.newInputStream(source)) {
+                byte[] buffer = new byte[8 * 1024];
+                int len;
+                while ((len = fileInputStream.read(buffer)) > 0) {
+                    zipOutputStream.write(buffer, 0, len);
+                }
+            }
+            zipOutputStream.closeEntry();
+        }
     }
 }
