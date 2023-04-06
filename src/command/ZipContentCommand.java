8b1f4dba5847e32d98e12494f36f19adb01aab06
kvit7 <kvit72v@yandex.ru>
2023-04-06 17:52:06 +0300

    Реализован метод execute() класса ZipContentCommand.

--- src/command/ZipContentCommand.java
+++ src/command/ZipContentCommand.java
@@ -1,8 +1,25 @@
 package command;
 
+import Archiver.ConsoleHelper;
+import Archiver.FileProperties;
+import Archiver.ZipFileManager;
+
+import java.util.List;
+
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
 
+        ZipFileManager zipFileManager = getZipFileManager();
+
+        ConsoleHelper.writeMessage("Содержимое архива:");
+
+        List<FileProperties> files = zipFileManager.getFilesList();
+        for (FileProperties file : files) {
+            ConsoleHelper.writeMessage(file.toString());
+        }
+
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
