ceff7e83469e4c2f02b067de973449b9289c27d0
kvit7 <kvit72v@yandex.ru>
2023-04-07 12:56:36 +0300

    В класс ZipFileManager довален метод extractAll. В классе ZipExtractCommand реализован метод execute().

--- src/command/ZipExtractCommand.java
+++ src/command/ZipExtractCommand.java
@@ -1,8 +1,27 @@
 package command;
 
+import Archiver.ConsoleHelper;
+import Archiver.ZipFileManager;
+import exception.WrongZipFileException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipExtractCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Распаковка архива.");
 
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите путь для распаковки:");
+            Path destinationPath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.extractAll(destinationPath);
+
+            ConsoleHelper.writeMessage("Архив был распакован.");
+        } catch (WrongZipFileException e) {
+            ConsoleHelper.writeMessage("Архив не существует.");
+        }
     }
 }
