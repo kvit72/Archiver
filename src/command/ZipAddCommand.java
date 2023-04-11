4ffe6f57ce9e44f12f8af3595af462a5822cbdeb
kvit7 <kvit72v@yandex.ru>
2023-04-11 11:42:52 +0300

    В класс ZipFileManager добавлены методы addFiles и addFile, которые отвечают за добавление файла в архив. В классе ZipAddCommand реализован метод execute.

--- src/command/ZipAddCommand.java
+++ src/command/ZipAddCommand.java
@@ -1,8 +1,29 @@
 package command;
 
+import Archiver.ConsoleHelper;
+import Archiver.ZipFileManager;
+import exception.PathlsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipAddCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Добавление нового файла в архив.");
+
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите полное имя файла для добавления:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+
+            zipFileManager.addFile(sourcePath);
+
+            ConsoleHelper.writeMessage("Добавление в архив завершено.");
+        } catch (PathlsNotFoundException e) {
+            ConsoleHelper.writeMessage("Файл не был найден.");
+        }
 
     }
 }
